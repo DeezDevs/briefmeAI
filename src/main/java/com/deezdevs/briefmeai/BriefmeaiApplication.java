9a748493377e97f8dd93e89a64e2c73b74203ca1
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-26 22:31:57 +0530

    initialized spring backend

---
+++ src/main/java/com/deezdevs/briefmeai/BriefmeaiApplication.java
@@ -0,0 +1,13 @@
+package com.deezdevs.briefmeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriefmeaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriefmeaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deezdevs/briefmeai/BriefmeaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deezdevs.briefmeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefmeaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
